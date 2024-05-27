ee46353cd63bceb38b7ae7fae9c59ec874aea3c1
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-05-27 15:15:51 +0200

    added posts controllers

--- routers/posts.js
+++ routers/posts.js
@@ -4,5 +4,8 @@ const express = require('express');
 // Istanza di express.Router()
 const router = express.Router();
 
+// Controller dei posts
+const postsControllers = require("../controllers/posts.js");
+
 // Esporto l'istanza di router
 module.exports = router;
