09ee6ead7d2f6576529007d66e2b427b32f48a32
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-05-27 16:01:36 +0200

    added posts show

--- controllers/posts.js
+++ controllers/posts.js
@@ -4,6 +4,7 @@ const { fs, path } = require("../utils");
 // Importo i posts
 let posts = require("../db/posts.json");
 
+// Index dei Posts
 const index = (req, res) => {
 
     // Content negotiation
@@ -36,7 +37,55 @@ const index = (req, res) => {
     });
 }
 
+// Show dei posts
+const show = (req, res) => {
+    // Recupero lo slug dai parametri
+    const requestPostSlug = req.params.slug;
+
+    // Provo a cercare se tra gli slug dei posts esiste una relazione
+    const requestPost = posts.find(post => post.slug === requestPostSlug);
+
+    //Logica HTML
+    res.format({
+        html: () => {
+            if (requestPost) {
+                const p = requestPost;
+                res.send(`
+                    <div>
+                        <h3>${p.title}</h3>
+                        <img width="300" height="200" src="/${p.image}" alt="${p.title}"/>
+                        <p>${p.content}</p>
+                        <p>${p.tags.map(t => `<strong><span class="tag">#${t}<span></strong>`).join(' ')}</p>
+                    </div>
+                `);
+            } else {
+
+                // Restituisco un 404 se il post non esiste
+                res.status(404).send(`<h1>Post non trovato</h1>`);
+            }
+        },
+
+        // Logica JSON
+        json: () => {
+
+            // Se esiste
+            if (requestPost) {
+                res.json({
+                    ...requestPost,
+                    image_url: `http://${req.headers.host}/${requestPost.image}`
+                });
+            } else {
+                res.status(404).json({
+                    error: 'Not Found',
+                    description: `Non esiste un post con slug ${slugrequestPost}`
+                });
+            }
+        }
+    });
+}
+
 // Esporto i moduli
 module.exports = {
-    index
+    index,
+    show
 }
--- routers/posts.js
+++ routers/posts.js
@@ -10,5 +10,8 @@ const postsControllers = require("../controllers/posts.js");
 // Index dei Posts
 router.get('/', postsControllers.index);
 
+// Show dei posts
+router.get("/:slug", postsControllers.show);
+
 // Esporto l'istanza di router
 module.exports = router;
